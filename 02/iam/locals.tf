8153335b603437c70dea3a3ca72017b001408a7a
Poonam Shelke <punamshelke.98@gmail.com>
2023-07-10 19:48:41 +0530

    Add. iam role for harness demo
    This role will be used by pipeline to perform operations in the AWS account. Currently it has only one permission to put object in the S3 bucket which will be used by pipeline to push the CI atifacts into the S3 bucket

---
+++ 02/iam/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  role_name          = "demo-role-for-harness"
+  policy_name        = "demo-policy-document-for-harness"
+  policy_description = "This policy allows the role to be assumed by the harness pipeline and perform the actions as needed"
+}
